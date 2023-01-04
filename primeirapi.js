bca88d877aac1f123203174fb3ae30c0b599e8e0
Gh-Cunha <ghcweb@hotmail.com>
2023-01-04 18:56:10 -0300

    att da Api e metodos.

--- primeirapi.js
+++ primeirapi.js
@@ -1,14 +1,36 @@
-// Primeira API
-// uso do modo interno do node o http
-
 let http = require('http')
 
 function server(request, response){
 
-    response.writeHead(200, {'Content-Type': 'application/json'})
-    response.write('Olá Mundo!')
-    response.end()
+    if(request.method === 'POST'){
+        response.writeHead(200, {'Content-Type': 'application/json'})
+            response.write('Você postou kk')
+            response.end()
+    }
 
+    if(request.method === 'GET'){
+       
+        if(request.url === '/hpro'){
+            
+            response.writeHead(200, {'Content-Type': 'application/json'})
+            response.write('Olá Mephyrus')
+            response.end()
+        }
+       
+        else if(request.url === '/gpro'){
+           
+            response.writeHead(200, {'Content-Type': 'application/json'})
+            response.write('Olá Nephyr')
+            response.end()
+        }
+
+        else{
+            response.writeHead(404)
+            response.write('Infelizmente não encontramos')
+            response.end()
+        }
+    }
 }
 
 http.createServer(server).listen(2000)
+ 
